bd47ad69a0fd2b197b3eb24ca59afba319a44284
Kevin Schoonover <me@kschoon.me>
2022-08-20 16:48:30 -0700

    http: clone http.DefaultTransport instead of global override

--- http/client.go
+++ http/client.go
@@ -9,15 +9,16 @@ import (
 // Create new http.Client with sensible defaults including
 // 1. SRV lookups with the '.consul' suffix with automatic protocol detection. For example,  https://tip.service.consul will do an SRV lookup
 func NewClient() *http.Client {
-	client := &http.Client{
-		Timeout:   time.Second * 10,
-		Transport: http.DefaultTransport,
-	}
-
-	client.Transport.(*http.Transport).DialContext = NewSRVDialer(&net.Dialer{
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.DialContext = NewSRVDialer(&net.Dialer{
 		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 	}).DialContext
+
+	client := &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: transport,
+	}
 
 	return client
 }
