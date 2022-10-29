79f7aa75bebb9c25d8c1b0b3f7f74a7aceaf7790
Kevin Schoonover <me@kschoon.me>
2022-10-29 01:40:54 -0700

    add auth0, nomad, and spaces config

---
+++ config/config_spaces.go
@@ -0,0 +1,88 @@
+package config
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws"
+)
+
+type DigitalOceanSpacesConfig struct {
+	sync.RWMutex
+
+	Region      string
+	Endpoint    string
+	AccessKeyID string `toml:"access_key_id"`
+	SecretKey   string `toml:"secret_key"`
+
+	client *s3.Client
+}
+
+func (c *DigitalOceanSpacesConfig) RegisterFlags(f *flag.FlagSet) {
+	flag.StringVar(&c.Region, "spaces.region", getenv("SPACES_REGION", "sfo3"), "Region to use in querys")
+	flag.StringVar(&c.Endpoint, "spaces.endpoint", getenv("SPACES_ENDPOINT", "digitaloceanspaces.com"), "Endpoint to use for queries")
+}
+
+func (c *DigitalOceanSpacesConfig) Merge(other *DigitalOceanSpacesConfig) error {
+	c.Region = other.Region
+	c.Endpoint = other.Endpoint
+	c.AccessKeyID = other.AccessKeyID
+	c.SecretKey = other.SecretKey
+
+	client, err := c.CreateClient()
+	if err != nil {
+		return err
+	} else {
+		c.client = client
+	}
+
+	return nil
+}
+
+func (c *DigitalOceanSpacesConfig) CreateClient() (*s3.Client, error) {
+	// setup s3 sdk for use with digitalocean + opentelemetry
+	resolver := aws.EndpointResolverWithOptionsFunc(func(service, awsRegion string, options ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			URL: fmt.Sprintf("https://%s.%s", c.Region, c.Endpoint),
+		}, nil
+	})
+	s3config, err := awsconfig.LoadDefaultConfig(
+		context.Background(),
+		awsconfig.WithEndpointResolverWithOptions(resolver),
+		awsconfig.WithDefaultRegion(c.Region),
+		awsconfig.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(c.AccessKeyID, c.SecretKey, "")),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	otelaws.AppendMiddlewares(&s3config.APIOptions)
+
+	return s3.NewFromConfig(s3config), nil
+}
+
+// Initialize Metrics + Tracing for the app. NOTE: you must call defer t.Stop() to propely cleanup
+func (c *DigitalOceanSpacesConfig) GetClient() (s3.Client, error) {
+	if c.client == nil {
+		client, err := c.CreateClient()
+		if err != nil {
+			return *client, err
+		}
+		c.Lock()
+		c.client = client
+		c.Unlock()
+
+		return *client, err
+	}
+
+	c.RLock()
+	defer c.RUnlock()
+	return *c.client, nil
+}
