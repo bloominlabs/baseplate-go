743a6985de3256b181af95ca4f53ead80f98feff
Kevin Schoonover <me@kschoon.me>
2023-02-27 00:25:35 -0800

    feat(auth0): propogate tracing + fix noamd.token

--- config/config_auth0.go
+++ config/config_auth0.go
@@ -2,9 +2,12 @@ package config
 
 import (
 	"flag"
+	"net/http"
 	"sync"
+	"time"
 
 	"github.com/auth0/go-auth0/management"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 const DefaultAuth0Domain = "https://hostin-proj.us.auth0.com"
@@ -21,7 +24,7 @@ type Auth0Config struct {
 
 func (c *Auth0Config) RegisterFlags(f *flag.FlagSet) {
 	flag.StringVar(&c.Domain, "auth0.domain", getenv("AUTH0_DOMAIN", DefaultAuth0Domain), "hostname:port to connect to the nomad server")
-	flag.StringVar(&c.Token, "noamd.token", getenv("AUTH0_TOKEN", ""), "Token to use to authenticate to auth0 (can be used instead of auth0.client_id + auth0.client_secret)")
+	flag.StringVar(&c.Token, "auth0.token", getenv("AUTH0_TOKEN", ""), "Token to use to authenticate to auth0 (can be used instead of auth0.client_id + auth0.client_secret)")
 	flag.StringVar(&c.ClientID, "auth0.client_id", getenv("AUTH0_CLIENT_ID", ""), "Auth0 Management Client ID to authenticate to auth0 (can be used instead of auth0.token)")
 	flag.StringVar(&c.ClientSecret, "auth0.client_secret", getenv("AUTH0_CLIENT_SECRET", ""), "Auth0 Management Client Secret with capability to create users (can be used ins tead of auth0.client_token")
 }
@@ -45,10 +48,13 @@ func (c *Auth0Config) Merge(other *Auth0Config) error {
 }
 
 func (c *Auth0Config) CreateClient() (*management.Management, error) {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	client := &http.Client{Transport: otelhttp.NewTransport(transport), Timeout: time.Minute}
+
 	if c.Token == "" {
-		return management.New(c.Domain, management.WithClientCredentials(c.ClientID, c.ClientSecret))
+		return management.New(c.Domain, management.WithClientCredentials(c.ClientID, c.ClientSecret), management.WithClient(client))
 	} else {
-		return management.New(c.Domain, management.WithStaticToken(c.Token))
+		return management.New(c.Domain, management.WithStaticToken(c.Token), management.WithClient(client))
 	}
 }
 
