432a859ec002cc4b05ff4adfe1fa7ccaf60d8610
Kevin Schoonover <me@kschoon.me>
2023-12-03 01:11:05 -0800

    feat(config/observability): add 'WithNomad' resource

--- config/observability/resource.go
+++ config/observability/resource.go
@@ -2,15 +2,33 @@ package observability
 
 import (
 	"context"
+	"os"
 
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
 )
+
+type nomad struct{}
+
+// Detect returns a *Resource that describes the host being run on.
+func (nomad) Detect(ctx context.Context) (*resource.Resource, error) {
+	return resource.NewSchemaless(
+		attribute.String("nomad.job.name", os.Getenv("NOMAD_JOB_NAME")),
+		attribute.String("nomad.alloc.id", os.Getenv("NOMAD_ALLOC_ID")),
+	), nil
+}
+
+// WithHost adds attributes from the host to the configured resource.
+func WithNomad() resource.Option {
+	return resource.WithDetectors(nomad{})
+}
 
 func WithDefaultResourceOpts() []resource.Option {
 	return []resource.Option{
 		resource.WithFromEnv(),
 		resource.WithTelemetrySDK(),
 		resource.WithHost(),
+		WithNomad(),
 	}
 }
 
